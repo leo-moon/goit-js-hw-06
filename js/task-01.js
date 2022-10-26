982cebdd16980fa068dac3ca98ac9060531bcca8
Vladimir Larin <lvv0820@gmail.com>
2022-10-26 12:15:54 +0300

    01 task

---
+++ js/task-01.js
@@ -0,0 +1,29 @@
+const getElementListSelectorRef = (element, selector) =>
+    element.querySelector(selector);
+
+const getUlListRef = getElementListSelectorRef(document,'#categories');
+const getUlListChildrenRef = getUlListRef.children;
+console.log('Number of categories:', getUlListChildrenRef.length);
+
+const categoryRef = li => {
+    const titleRef = getElementListSelectorRef(li, 'h2');
+    console.log('Category:', titleRef.textContent);
+
+    const ulInLi = getElementListSelectorRef(li, 'ul');
+    console.log('Elements:', ulInLi.children.length);
+}
+
+for (let i = 0; i < getUlListChildrenRef.length; i++) {
+categoryRef(getUlListChildrenRef[i]);
+}
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
