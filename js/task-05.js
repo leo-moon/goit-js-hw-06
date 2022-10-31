3a500c090e1b58d43370f8f8535cebf644671925
Vladimir Larin <lvv0820@gmail.com>
2022-10-31 15:55:56 +0200

    05

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,14 @@ const getElementListSelectorRef = (selector, element = document) =>
     element.querySelector(selector);
 
 const nameRef = getElementListSelectorRef('input');
+const nameOutputRef = getElementListSelectorRef('#name-output');
+const nameOutputRefAnonimus = nameOutputRef.textContent;
 
+const onInputCheck = () => {
+        console.log(nameRef.value);
+        nameOutputRef.textContent = nameRef.value != '' ? 
+            nameRef.value: nameOutputRefAnonimus ;
+}
 
-
-console.log(nameRef.value, nameRef);
+const onInput = nameRef.addEventListener('input', onInputCheck);
+// console.log(nameRef.value, nameRef);
