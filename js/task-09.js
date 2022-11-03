93cb93e16ae465fd6e54adcf8a68e89f759c5d88
Vladimir Larin <lvv0820@gmail.com>
2022-11-03 22:50:06 +0200

    09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+  color: '#ffffff',
+};
+
+const changeBodyColor = () => {
+  document.body.style.background = refs.color;
+  console.log(document.body.style.background)
+};
+
+const writeColor = () => {
+  refs.span.textContent = refs.color;
+};
+
+const changeBGC = () => {
+  refs.color = getRandomHexColor();
+  changeBodyColor();
+  writeColor();
+};
+
+refs.button.addEventListener('click', changeBGC);
